6ca7eed90133dbd1a3aff1819256e639b138fbed
Tom <tom.aschmann@t-online.de>
2024-09-14 11:46:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-    if ($_GET['vpncode']) {
-      if(strlen($_GET['vpncode']) == 6) {
+    if (isset($_GET['vpncode']) && strlen($_GET['vpncode']) == 6 && isset($_GET['day']) && is_numeric($_GET['day']) && intval($_GET['day']) >= 1 && intval($_GET['day']) <= 14) {
         $vpncode = htmlspecialchars($_GET['vpncode']);
 
         // Wenn der Button geklickt wurde
@@ -24,15 +23,14 @@
           <button type="submit" name="submit">Bestätigen</button>
         </form>
         <?php
-
-      } else {
-        echo "Error 1: Der VPN-Code ist ungültig";
-      }
     } else {
       ?>
         <form method="get">
           <label for="vpncode">Bitte gebe deinen VPN-Code ein, welchen du auch im Fragebogen eingegeben hast. Dieser setzt sich zusammen aus ###########
-              <input type="text" id="vpncode" name="vpncode" placeholder="VPN-Code" minlength="6" maxlength="6">
+              <input type="text" id="vpncode" name="vpncode" placeholder="VPN-Code" minlength="6" maxlength="6" required>
+          </label
+          <label for="day">
+            <input type="number" id="day" placeholder="Tag" name="day" min=1 max=14 minlength="1" maxlength="2" required>
           </label>
           <label for="send_form">
             <input type="submit" value="Senden" id="send_form">
