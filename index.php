4ddc6b2033fc7445af61a61b77d3f02908d49699
Tom <tom.aschmann@t-online.de>
2024-09-20 11:56:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,26 @@
     require './header.inc.php'; # Header Datei, in welcher beginnende Inhalte gespeichert sind, die auf jeder Seite anfangs eingebunden werden
     if (isset($_GET['vpncode']) && strlen($_GET['vpncode']) == 6 && is_numeric($_GET['group']) && (intval($_GET['group']) == 1 || intval($_GET['group']) == 2)) { # Abfrage der immer benötigten GET-Parameter
         if (isset($_GET['register']) && !isset($_GET['day'])) { # Abfrage ob Person registriert werden soll (und Ausschluss des day-GET-Parameters)
-
             ?>
+            <h2>Dies ist die Anmeldung für die Intervention zur Selbstakzeptanz.</h2>
+            <h4>Bitte gebe deine E-Mail-Adresse unten ein. Du bekommst eine automatisch E-Mail zugesendet.</h4>
+            <?php
+            if (intval($_GET['group']) = 1) { # Interventionsgruppe
+              ?>
+              <p>Du bekommst 10 Tage lang jeden Tag morgens und Abends eine kleine Aufgabe. Dies dauert nur 5 Minuten und hilft dir deine Selbstakzeptanz zu erhöhen.</p>
+              <?php
+            } elseif (intval($_GET['group']) = 2) { # Kontrollgruppe
+              ?>
+              <p>Du bekommst eine Aufgabe zur per E-Mail zugesendet. Diese hilft dir deine Selbstakzeptanz zu erhöhen.</p>
+              <?php
+            }
+            ?>
+              <p>
+                Nach 10 Tagen bekommst du eine weitere E-Mail mit einem Link für den zweiten Fragebogen. <strong>Dieser zweite Fragebogen ist sehr wichtig!</strong><br>
+                Als <strong>Dankeschön<strong> kannst du, nach Abschluss der Intervention und des zweiten Fragebogens, an einer Verlosung von 3x 10 € Wunschgutscheinen teilnehmen, bekommst eine 3-monatige gratis Premium Mitgliedschaft der Meditationsapp 7mind und bekommst Zugang zu der fertigen Studie.<br>
+                Als Student der HSRW bekommst du dann auch deine <!-- 3,5? --> Versuchspersonenstunden gutgeschrieben.
+                Mit deiner Teilnahme leistest du einen großen Beitrag zur Wissenschaft und unterstützt mich sehr bei meiner Bachelorarbeit. Vielen Dank!
+              </p>
               <form method="post" action="?<?php echo htmlspecialchars($_SERVER['QUERY_STRING']); ?>">
                   <label for="email">
                     <input type="email" id="email" name="email" placeholder="E-Mail-Adresse">
