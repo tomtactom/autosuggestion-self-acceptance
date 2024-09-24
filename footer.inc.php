5abcf8b05142498c21fa4a6115d3295bcd969588
Tom <tom.aschmann@t-online.de>
2024-09-24 16:07:37 +0200

    Update footer.inc.php

--- footer.inc.php
+++ footer.inc.php
@@ -116,7 +116,7 @@
 <!-- JavaScript-Bibliotheken -->
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="./assets/timer.js"></script> <!-- JavaScript 4-Minuten Timer für die Übung -->
 <script>
     // Schutz für die E-Mail-Adresse
