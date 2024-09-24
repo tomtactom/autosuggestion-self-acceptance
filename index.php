45aa29c27cb7e7a0fd7db830822c4036ee83da3d
Tom <tom.aschmann@t-online.de>
2024-09-24 18:21:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -174,7 +174,7 @@
               // Formular erstellen
               ?>
               <div class="container">
-                  <div class="accordion" id="selfAcceptanceAccordion">
+                    <div class="accordion" id="selfAcceptanceAccordion">
                       <div class="accordion-item">
                           <h2 class="accordion-header" id="headingOne">
                               <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" style="color:grey;">
