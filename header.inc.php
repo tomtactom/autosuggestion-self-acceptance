ad48d657ffde9da7469bdf8a6169796cb32c6682
Tom <tom.aschmann@t-online.de>
2024-09-24 17:29:21 +0200

    pooper

--- header.inc.php
+++ header.inc.php
@@ -67,6 +67,7 @@ if ($table_check !== false && $table_check->num_rows == 0) {
 
       <!-- JavaScript-Dateien -->
       <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
+      <script src="https://unpkg.com/@popperjs/core@2"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
       <title>Selbstakzeptanz Autosuggestion</title>
